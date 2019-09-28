f6f73df7e9c136f78c580ecda417af8580434ead
Student <student@itpark>
2019-09-28 10:57:21 +0300

    переименовали автора

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 console.log('worked')
 
-const author = {
+const buzova = {
     id: 'buzova86',
     avatarUrl: https://scontent-arn2-1.cdninstagram.com/vp/e24c6f401983834e27247c3542022660/5E1B7CB0/t51.2885-19/s150x150/69672885_368328154091538_8485268536803983360_n.jpg?_nc_ht=scontent-arn2-1.cdninstagram.com
     verified: true,
 }
 const photo = {
     id: 'B32d882NooNF',
-    author: author,
+    author: buzova,
     location: 'Набережная Амура',
     imageUrl: 'https://placekitten.com/800/800',
     videoUrl: null,
@@ -16,7 +16,7 @@ const photo = {
 
 const video = {
     id: 'B32d882NNF',
-    author: author,
+    author: buzova,
     location: 'ТК Центр острова',
     imageUrl: 'https://placekitten.com/800/800',
     videoUrl: 'https://youtube.com/...',
